f5b6a34c77b5b7f1217d0eebaf53a68665e4ae46
Hudson Ferreira <hudsonferreiramb@hotmail.com>
2025-05-06 22:51:44 -0300

    rollback

--- keep_alive.py
+++ keep_alive.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from threading import Thread
-import os
-import asyncio
-from monitor_userbot import main  # Importa o loop principal do bot
+import os  # Adicionar esta linha
 
 app = Flask('')
 
@@ -16,11 +14,5 @@ def run():
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
-    # Inicia o servidor Flask
     t = Thread(target=run)
     t.start()
-
-# Inicia o bot do Telethon
-if __name__ == "__main__":
-    keep_alive()
-    asyncio.run(main())  # Executa o loop principal do bot
